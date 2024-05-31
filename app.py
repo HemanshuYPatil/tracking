bc0d08719c59bd5b3de3b44c7e85c3b1cb8e70e8
Hemanshu Patil <110720608+HemanshuYPatil@users.noreply.github.com>
2024-06-01 01:17:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 import requests
 from opencage.geocoder import OpenCageGeocode
+import os
 
 app = Flask(__name__)
 
@@ -16,7 +17,8 @@ def get_coordinates(place_name):
 def get_phone_location():
     phone_number = request.args.get('phone_number')
     API_KEY = '87a87dbc1a7b67052f8a3a4d7e1ca4f7'
-    url = f'http://apilayer.net/api/validate?access_key={API_KEY}&number={phone_number}&country_code=&format=1'
+    # Construct URL using url_for
+    url = url_for('validate_number', _external=True, access_key=API_KEY, number=phone_number)
     response = requests.get(url)
     data = response.json()
     if data['valid']:
@@ -26,5 +28,13 @@ def get_phone_location():
     else:
         return jsonify({'error': 'Invalid phone number'}), 400
 
+@app.route('/validate_number')
+def validate_number():
+    access_key = request.args.get('access_key')
+    number = request.args.get('number')
+    url = f'http://apilayer.net/api/validate?access_key={access_key}&number={number}&country_code=&format=1'
+    response = requests.get(url)
+    return response.json()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
+    app.run()
